d0fc9f519467a0dc9025adc029e1d770f7fbada3
Javier <3289457+JavierZunzunegui@users.noreply.github.com>
2025-10-08 15:08:31 +0200

    adding zgen.IsType and zgen.Cast

--- util.go
+++ util.go
@@ -1,6 +1,22 @@
 package zgen
 
-// TODO - document.
+// PtrTo is a lazy way to do `var x T; &x`, but do so in a sigle statement.
 func PtrTo[T any](t T) *T {
 	return &t
+}
+
+// IsType is a lazy way to check if the value in an interface can be cast to another type.
+// Note if [T] is an interface the check is for 'can cast', if it is a concrete type the check
+// is for type equality.
+// See also the very simialr [Cast].
+func IsType[T any](i any) bool {
+	_, ok := i.(T)
+	return ok
+}
+
+// Cast is similar to [IsType] except it also returns the type it was casted to.
+// Note when returning false, the casted value is the T zero value.
+func Cast[T any](i any) (T, bool) {
+	t, ok := i.(T)
+	return t, ok
 }
